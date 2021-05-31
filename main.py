00feee05552cc2f994e349005d972dce78613168
Romain Deville <code@romaindeville.fr>
2021-05-31 14:12:00 +0200

    ✨ Add generation of html from template

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ try:
 except ImportError:
     from yaml import Loader, Dumper
 import gettext
+import markdown
 
 import jinja2
 from jinja2 import Template
@@ -53,9 +54,23 @@ def relative_delta_date(end,start):
     return relativedelta(end, start)
 
 
+@jinja2.contextfunction
+def get_context(c):
+        return c
+
+def subs(string,context):
+    return context[string]
+
+def to_html(string):
+    return markdown.markdown(string)
+
+def to_latex(string):
+    md = markdown.Markdown(extensions=['latex'])
+    return md.convert(string)
+
 def build_tex():
     latex_jinja_env = jinja2.Environment(
-        extensions=['jinja2.ext.i18n','jinja2.ext.with_'],
+        extensions=['jinja2.ext.i18n','jinja2.ext.with_','jinja2.ext.loopcontrols'],
         block_start_string = '[%',
         block_end_string = '%]',
         variable_start_string = '[[',
@@ -64,7 +79,7 @@ def build_tex():
         comment_end_string = '#]',
         trim_blocks = False,
         autoescape = False,
-        loader = jinja2.FileSystemLoader(os.path.join(os.path.abspath(BASEDIR),"tex","template"))
+        loader = jinja2.FileSystemLoader(os.path.join(os.path.abspath(BASEDIR),"template","tex"))
     )
     latex_jinja_env.install_gettext_callables(
             gettext=gettext.gettext,
@@ -76,19 +91,91 @@ def build_tex():
     latex_jinja_env.globals['iso_date'] = iso_date
     latex_jinja_env.globals['now_date'] = now_date
     latex_jinja_env.globals['relative_delta_date'] = relative_delta_date
+    latex_jinja_env.globals['subs'] = subs
+    latex_jinja_env.globals['context'] = get_context
+    latex_jinja_env.globals['to_latex'] = to_latex
 
-    for i_locale in LOCALES:
-        config=dict()
-        for i_file in os.listdir(os.path.join(BASDIR,"data",i_locale)):
-            with open(i_file) as config_file:
-                config.update(yaml.load(config_file, Loader=yaml.SafeLoader))
-        # Load the translations for the current locale
-        translations = Translations.load(LOCALE_PATH, [i_locale])
-        locale.setlocale(locale.LC_ALL, f"{i_locale}.UTF-8")
-        latex_jinja_env.install_gettext_translations(translations)
-        template = latex_jinja_env.get_template('resume.tex.j2')
-        render = template.render(config)
-        with open(os.path.join(BASEDIR,"tex",f"resume.{i_locale}.tex"),"w") as output_file:
-            output_file.write(render)
+    config=dict()
+    curr_file = os.path.join(BASEDIR,"data","locale.yaml")
+    with open(curr_file) as config_file:
+        config.update(yaml.load(config_file, Loader=yaml.SafeLoader))
 
-build_tex
+    for i_locale in config["locale"]:
+        latex_jinja_env.globals['locale'] = i_locale["code"]
+        config[i_locale["code"]] = dict()
+        if os.path.isdir(os.path.join(BASEDIR,"data",i_locale)):
+            for i_file in os.listdir(os.path.join(BASEDIR,"data",i_locale)):
+                curr_file = os.path.join(BASEDIR,"data",i_locale,i_file)
+                with open(curr_file) as config_file:
+                    config[i_locale].update(yaml.load(config_file, Loader=yaml.SafeLoader))
+            # Load the translations for the current locale
+            translations = Translations.load(LOCALE_PATH, [i_locale])
+            locale.setlocale(locale.LC_ALL, f"{i_locale}.UTF-8")
+            latex_jinja_env.install_gettext_translations(translations)
+            template = latex_jinja_env.get_template('resume.tex.j2')
+            render = template.render(config[i_locale['code']])
+            with open(os.path.join(BASEDIR,"tex",f"resume.{i_locale}.tex"),"w") as output_file:
+                output_file.write(render)
+
+def build_html():
+    latex_jinja_env = jinja2.Environment(
+        extensions=['jinja2.ext.i18n','jinja2.ext.with_','jinja2.ext.loopcontrols'],
+        trim_blocks = False,
+        autoescape = False,
+        loader = jinja2.FileSystemLoader(os.path.join(os.path.abspath(BASEDIR),"template","html"))
+    )
+    latex_jinja_env.install_gettext_callables(
+            gettext=gettext.gettext,
+            ngettext=gettext.ngettext,
+            newstyle=True
+    )
+    latex_jinja_env.globals['location'] = location
+    latex_jinja_env.globals['format_date'] = format_date
+    latex_jinja_env.globals['iso_date'] = iso_date
+    latex_jinja_env.globals['now_date'] = now_date
+    latex_jinja_env.globals['relative_delta_date'] = relative_delta_date
+    latex_jinja_env.globals['subs'] = subs
+    latex_jinja_env.globals['context'] = get_context
+    latex_jinja_env.globals['to_html'] = to_html
+
+    config=dict()
+    all_locale = dict()
+    curr_file = os.path.join(BASEDIR,"data","locale.yaml")
+    with open(curr_file) as config_file:
+        all_locale.update(yaml.load(config_file, Loader=yaml.SafeLoader))
+    config = all_locale
+
+    for i_locale in config["locale"]:
+        latex_jinja_env.globals['locale'] = i_locale["code"]
+        config[i_locale["code"]] = {
+            "locale": i_locale,
+            "all_locale": all_locale
+        }
+        output_dir = os.path.join(BASEDIR,"html",i_locale["code"])
+        if not os.path.isdir(output_dir):
+            os.mkdir(output_dir)
+        if os.path.isdir(os.path.join(BASEDIR,"data",i_locale["code"])):
+            for i_file in os.listdir(os.path.join(BASEDIR,"data",i_locale["code"])):
+                curr_file = os.path.join(BASEDIR,"data",i_locale["code"],i_file)
+                with open(curr_file) as config_file:
+                    config[i_locale["code"]].update(yaml.load(config_file, Loader=yaml.SafeLoader))
+            # Load the translations for the current locale
+            translations = Translations.load(LOCALE_PATH, [i_locale["code"]])
+            locale.setlocale(locale.LC_ALL, f"{i_locale['code']}.UTF-8")
+            latex_jinja_env.install_gettext_translations(translations)
+            template = latex_jinja_env.get_template('index.html.j2')
+            render = template.render(config[i_locale['code']])
+            with open(os.path.join(output_dir,f"index.html"),"w") as output_file:
+                output_file.write(render)
+
+            template = latex_jinja_env.get_template('404.html.j2')
+            render = template.render(config[i_locale['code']])
+            with open(os.path.join(output_dir,f"404.html"),"w") as output_file:
+                output_file.write(render)
+
+
+def main():
+    #build_tex()
+    build_html()
+
+main()
