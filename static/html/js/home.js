64c63a53e21ed2185779654c3f3227567f640a77
Romain Deville <code@romaindeville.fr>
2021-06-07 14:10:01 +0200

    ✨ Add data-privacy popup css and js

--- static/html/js/home.js
+++ static/html/js/home.js
@@ -199,3 +199,14 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+function openDataPrivacy() {
+    var cdiv = document.getElementById("data-privacy-overlay");
+    cdiv.setAttribute("class","d-block")
+}
+
+function closeDataPrivacy() {
+    var cdiv = document.getElementById("data-privacy-overlay");
+    cdiv.setAttribute("class","d-none")
+}
+
