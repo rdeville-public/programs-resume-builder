568ffedde089d20823ccd7280e17b8ccbbbf2c5c
Romain Deville <code@romaindeville.fr>
2021-06-10 12:44:12 +0200

    ✏ Fix typo in konami.js

--- static/html/js/konami.js
+++ static/html/js/konami.js
@@ -88,17 +88,18 @@ function activateCheats() {
 }
 
 function cycleFrames (_nyanCat, _currentFrame) {
-	_nyanCat.classList = []
-	_nyanCat.classList.add(`frame${_currentFrame}`)
+  _nyanCat.classList = []
+  _nyanCat.classList.add(`frame${_currentFrame}`)
 }
 
 (function () {
-	let nyanCat = document.getElementById('nyan-cat')
-	let currentFrame = 1
+  let nyanCat = document.getElementById('nyan-cat')
+  let currentFrame = 1
 
-	setInterval(function () {
-		currentFrame = (currentFrame % 6) + 1
-		cycleFrames(nyanCat, currentFrame)
-	}, 70)
+  setInterval(function () {
+    currentFrame = (currentFrame % 6) + 1
+    cycleFrames(nyanCat, currentFrame)
+  }, 70)
 })()
+
 
